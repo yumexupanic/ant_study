c893c8a99dfda4a8dfbcf3018163ef8529f3b0f0
justpsvm <justpsvm@gmail.com>
2019-02-18 17:55:11 +0800

    Dva 使用

---
+++ day04/src/model/puzzlecards.js
@@ -0,0 +1,35 @@
+export default {
+    namespace: 'puzzlecards',
+    state: {
+        data: [
+            {
+                id: 1,
+                setup: 'Did you hear about the two silk worms in a race????',
+                punchline: 'It ended in a tie',
+            },
+            {
+                id: 2,
+                setup: 'What happens to a frog\'s car when it breaks down?',
+                punchline: 'It gets toad away',
+            },
+        ],
+        counter: 100,
+    },
+    effects: {
+        *queryInitCards(_,sagaEffects){
+            const { call, put } = sagaEffects;
+
+        }
+    },
+    reducers: {
+        addNewCard(state, { payload: newCard }) {
+            const nextCounter = state.counter + 1;
+            const newCardWithId = { ...newCard, id: nextCounter };
+            const nextData = state.data.concat(newCardWithId);
+            return {
+              data: nextData,
+              counter: nextCounter,
+            };
+        }
+    }
+}
